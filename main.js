84726c69d24978a5b6ebf129981e11d2ac0b0e23
Darya Novikava <dashasaldunova@gmail.com>
2024-08-21 11:01:48 +0200

    modified main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import Card from "/class-card.js";
-import AmazingCard from "/class-amazing-card.js";
+import Card from './class-card';
+import AmazingCard from './class-amazing-card';
 
 let numberOfPairs = null;
 let firstCard = null;
